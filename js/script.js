d7e7b7dce91a3a5f1b36da6a0d4091a7ade6646f
TremMT <tremariet@gmail.com>
2023-07-03 11:44:29 -0700

    JS update - kitty hide and show

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-var button = document.querySelector(".show-cat");
+/*var button = document.querySelector(".show-cat");
+//console.log(button);
+
 var cat = document.querySelector(".cat");
-button.addEventListener("click", function() {
-    cat.classList.add("show");
-});
 //console.log(cat);
+
+button.addEventListener("click", function () {
+  cat.classList.add("show");
+});
+//console.log("It works!");*/
+
+//Adding If and Else Statements
+var button = document.querySelector(".show-cat");
+
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function () {
+  //cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+    //  console.log("Yes");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+  }
+  //  console.log("No..");
+});
